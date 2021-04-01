c532ac1db94c2b83199ef34a8c4a7bf4ed2a3ee9
Anna <gosteeva.2018@stud.nstu.ru>
2021-04-01 22:50:27 +0700

    Добавьте файлы проекта.

---
+++ Compiler/Automat.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    class PascalCommentFinder
+    {
+        private static char state;
+
+        private static bool IsM(char symbol)
+        {
+            if (symbol == '('  || symbol == ')' || symbol == '*' || symbol == '{' || symbol == '}')
+                return true;
+            else
+                return false;
+        }
+        private static bool Isa(char symbol)
+        {
+            return !IsM(symbol);
+        }
+        private static bool Check(string input, char state)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (state)
+                {
+                    case 'I': { if (input[i] == '{') state = 'K'; else if (input[i] == '(') state = 'D'; else return false; break; }
+                    case 'K':
+                        {
+                            if (input[i] == '}')
+                                return Check(input.Substring(i + 1, input.Length - i - 1), 'K') || Check(input.Substring(i + 1, input.Length - i - 1), 'F');
+                            else state = 'K'; break;
+                        }
+                    case 'D': { if (input[i] == '*') state = 'C'; else return false; break; }
+                    case 'C':
+                        {
+                            if (input[i] == '*')
+                                return Check(input.Substring(i + 1, input.Length - i - 1), 'C') || Check(input.Substring(i + 1, input.Length - i - 1), 'E');
+                            else state = 'C'; break;
+                        }
+                    case 'E': { if (input[i] == ')') state = 'F'; else return false; break; }
+                    default:
+                        return false;
+                }
+            }
+            if (state == 'F')
+                return true;
+            else
+                return false;
+        }
+        public static bool CheckString(string input)
+        {
+            return Check(input, 'I');
+        }
+    }
+}
